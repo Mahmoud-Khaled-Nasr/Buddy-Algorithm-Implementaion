27b3c2ba70af599189ad4ea24e24aa6f16bbc1fb
MK <mahmoud.k.nasr@gmail.com>
2017-12-22 15:13:44 +0200

    Modify the format of the log file

--- process.cpp
+++ process.cpp
@@ -44,13 +44,13 @@ int process::run(int quanta, int time) {
 	out << "Executing process " << id << "\t: started at " << time << ", \t";
 	if (remaining_run_time > quanta) {
 		remaining_run_time -= quanta;
-		out << "stopped at " << time + quanta << ", " << remaining_run_time << " remaining, memory startes at "
+		out << "stopped at " << time + quanta << ", " << remaining_run_time << " remaining, memory starts at "
 			<< location.first << " and ends at " << location.second << endl;
 		return quanta ;
 	} else {
 		int temp = remaining_run_time;
 		remaining_run_time = 0;
-		out << "finished at " << time + temp << " ,memory startes at "
+		out << "finished at " << time + temp << ", memory starts at "
 			<< location.first << " and ends at " << location.second << endl;
 		return temp ;
 	}
